81deffa227e7f2eb121b1896e90d4a8a8213d4f7
wyh <wb-wyh386287@alibaba-inc.com>
2024-01-04 10:46:25 +0800

    文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,66 +12,116 @@ module.exports = {
     ['link', { rel: 'icon', href: 'favicon.png' }]
   ],
   themeConfig: {
-    lastUpdated: 'Last Updated', // string | boolean
+    // logo: '/logo.svg',
+    // docsDir: 'docs',
+    lastUpdated: '最后更新',
     subSidebar: 'auto',
     nav: [
-      { text: '首页', link: '/' },
-      { text: '文档', link: '/docs/url' },
       {
         text: 'Github',
         link: 'https://github.com/wyh-code/utils'
       }
     ],
-    sidebar: {
-      '/docs/': [
-        '/docs/url',
-        '/docs/date',
-        '/docs/assets',
-        '/docs/storage',
-        '/docs/format',
-        '/docs/math',
-        '/docs/dom',
-        '/docs/colors',
-        '/docs/utils'
-      ]
-    }
-    // [
-    //   { 
-    //     title: "路由", 
-    //     path: "/docs/url"
-    //   },
-    //   { 
-    //     title: "时间", 
-    //     path: "/docs/date"
-    //   },
-    //   { 
-    //     title: "资源类", 
-    //     path: "/docs/assets"
-    //   },
-    //   { 
-    //     title: "本地缓存", 
-    //     path: "/docs/storage"
-    //   },
-    //   { 
-    //     title: "格式化", 
-    //     path: "/docs/format"
-    //   },
-    //   { 
-    //     title: "数学计算", 
-    //     path: "/docs/math"
-    //   },
-    //   { 
-    //     title: "DOM", 
-    //     path: "/docs/dom"
-    //   },
-    //   { 
-    //     title: "颜色值", 
-    //     path: "/docs/colors"
-    //   },
-    //   { 
-    //     title: "其他", 
-    //     path: "/docs/utils"
-    //   },
-    // ]
+    sidebar: [
+      {
+        title: '介绍',
+        path: '/',
+        collapsable: false,
+        children: [
+          {
+            title: '关于',
+            path: '/',
+          }
+        ]
+      },
+      {
+        title: '核心',
+        path: '/docs/url',
+        collapsable: false,
+        children: [
+            { 
+              title: "路由", 
+              path: "/docs/url"
+            },
+            { 
+              title: "时间", 
+              path: "/docs/date"
+            },
+            { 
+              title: "资源类", 
+              path: "/docs/assets"
+            },
+            { 
+              title: "本地缓存", 
+              path: "/docs/storage"
+            },
+            { 
+              title: "格式化", 
+              path: "/docs/format"
+            },
+            { 
+              title: "数学计算", 
+              path: "/docs/math"
+            },
+            { 
+              title: "DOM", 
+              path: "/docs/dom"
+            },
+            { 
+              title: "颜色值", 
+              path: "/docs/colors"
+            },
+            { 
+              title: "其他", 
+              path: "/docs/utils"
+            },
+          ]
+      }
+    ],
+    plugins: [
+      ['@vuepress-reco/vuepress-plugin-bulletin-popover', {
+        width: '300px', // 默认 260px
+        title: '消息提示',
+        body: [
+          {
+            type: 'title',
+            content: '添加冴羽好友入前端交流群',
+            style: 'text-aligin: center;'
+          },
+          {
+            type: 'image',
+            src: 'https://cdn.jsdelivr.net/gh/mqyqingfeng/picture/IMG_3516.JPG'
+          }
+        ],
+        footer: [
+          {
+            type: 'button',
+            text: '打赏',
+            link: '/donate'
+          } 
+        ]
+      }],
+      ["vuepress-plugin-nuggets-style-copy", {
+        copyText: "复制代码",
+        tip: {
+            content: "复制成功"
+        }
+     }]
+    ]
+    
   }
 }
+
+ // sidebar: {
+    //   '/docs/': [
+    //     '/docs/url',
+    //     '/docs/date',
+    //     '/docs/assets',
+    //     '/docs/storage',
+    //     '/docs/format',
+    //     '/docs/math',
+    //     '/docs/dom',
+    //     '/docs/colors',
+    //     '/docs/utils'
+    //   ]
+    // },
--- docs/README.md
+++ docs/README.md
@@ -1,17 +1,30 @@
 ---
-home: true
-heroImage: /favicon.png
-heroText: utils
-tagline: 一个常用的工具函数库
-actionText: 快速上手 →
-actionLink: /zh/guide/
-features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+# home: true
+# heroImage: /favicon.png
+# heroText: utils
+# tagline: 一个常用的工具函数库
+# actionText: 快速上手 →
+# actionLink: /zh/guide/
+# features:
+# - title: 简洁至上
+#   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+# - title: Vue驱动
+#   details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+# - title: 高性能
+#   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+# footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
 
+## 什么是 @ostore/utils
+  `@ostore/utils` 是一个`JavaScript`工具函数库，其名称源自于开发社区中常用的警句“不要重复造轮子（Don't reinvent the wheel）”。`ostore`的`o`源于轮子的形状，象征着实用性和多功能性，而 `store` 则意味着这是一个功能集合的仓库。其中，`utils`汇集了常用的函数功能。
+
+  `ostore`的宗旨是集成那些经过时间检验、广泛使用的通用功能，以避免开发者在日常编程中浪费时间去重新编写这些基本组件。
+
+## 关注
+  欢迎关注公众号，了解`@store/utils`更新动态及其他项目信息。
+
+## 交流
+  欢迎添加作者微信，交流学习。
+
+
+<!--  -->
